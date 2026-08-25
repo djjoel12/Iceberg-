b5ea418595a36ff10878a7a2720d2a0fa0ef92fb
djjoel12 <familledan12@gmail.com>
2026-08-25 15:00:53 +0000

    Update Map.tsx

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,41 +1,125 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-const position: [number, number] = [5.3364, -4.0267];
-
-const markerIcon = L.icon({
+const startIcon = L.icon({
   iconUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  iconRetinaUrl:
-    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
   shadowUrl:
     "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
 });
 
-export default function Map() {
+const endIcon = L.icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+});
+
+type Point = {
+  lat: number;
+  lng: number;
+  name?: string;
+};
+
+type Props = {
+  start: Point | null;
+  end: Point | null;
+  routeGeometry?: [number, number][];
+  onMapClick?: (lat: number, lng: number) => void;
+};
+
+function ClickHandler({
+  onMapClick,
+}: {
+  onMapClick?: (lat: number, lng: number) => void;
+}) {
+  useMapEvents({
+    click(e) {
+      onMapClick?.(e.latlng.lat, e.latlng.lng);
+    },
+  });
+  return null;
+}
+
+function FitBounds({ start, end }: { start: Point | null; end: Point | null }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (start && end) {
+      map.fitBounds(
+        [
+          [start.lat, start.lng],
+          [end.lat, end.lng],
+        ],
+        { padding: [50, 50] }
+      );
+    } else if (start) {
+      map.setView([start.lat, start.lng], 14);
+    } else if (end) {
+      map.setView([end.lat, end.lng], 14);
+    }
+  }, [start, end, map]);
+
+  return null;
+}
+
+export default function Map({ start, end, routeGeometry, onMapClick }: Props) {
   return (
     <MapContainer
-      center={position}
+      center={[5.3364, -4.0267]}
       zoom={12}
       scrollWheelZoom
-      style={{
-        width: "100%",
-        height: "400px",
-      }}
+      style={{ width: "100%", height: "400px" }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
 
-      <Marker position={position} icon={markerIcon}>
-        <Popup>📍 Abidjan</Popup>
-      </Marker>
+      <ClickHandler onMapClick={onMapClick} />
+      <FitBounds start={start} end={end} />
+
+      {start && (
+        <Marker position={[start.lat, start.lng]} icon={startIcon}>
+          <Popup>
+            <strong>Départ</strong>
+            <br />
+            {start.name || "Point de départ"}
+          </Popup>
+        </Marker>
+      )}
+
+      {end && (
+        <Marker position={[end.lat, end.lng]} icon={endIcon}>
+          <Popup>
+            <strong>Arrivée</strong>
+            <br />
+            {end.name || "Destination"}
+          </Popup>
+        </Marker>
+      )}
+
+      {routeGeometry && routeGeometry.length > 1 && (
+        <Polyline positions={routeGeometry} color="#111827" weight={5} opacity={0.85} />
+      )}
     </MapContainer>
   );
 }
